68f6763860d5d87e36d8728e7e4b35c08e174572
JUNSEO <djawnstj44@naver.com>
2023-06-30 17:25:51 +0900

    #10 - Bean name 과 Class 를 프로퍼티로 가진 BeanInfo 객체 생성 빈을 저장할 때 빈 이름도 같이 비교하기 위해서 기능 추가 Class로 동일성 비교하도록 오버라이딩

---
+++ src/main/java/com/project/winter/beans/BeanInfo.java
@@ -0,0 +1,26 @@
+package com.project.winter.beans;
+
+import java.util.Objects;
+
+public class BeanInfo {
+    private final String beanName;
+    private final Class<?> clazz;
+
+    public BeanInfo(String beanName, Class<?> clazz) {
+        this.beanName = beanName;
+        this.clazz = clazz;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BeanInfo beanInfo = (BeanInfo) o;
+        return Objects.equals(clazz, beanInfo.clazz);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(beanName, clazz);
+    }
+}
