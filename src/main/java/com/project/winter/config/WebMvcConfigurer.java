f40643aaab5bbf9f350ee1c4a8c9a4646b59149c
JUNSEO <djawnstj44@naver.com>
2023-07-31 17:13:16 +0900

    #21 - WebMvcConfigurer 수정
    이용자가 정의한 HandlerExceptionResolver 를 추가할 수 있도록 WebMvcConfigurer 에 configureHandlerExceptionResolvers 메서드를 추가했다.

--- src/main/java/com/project/winter/config/WebMvcConfigurer.java
+++ src/main/java/com/project/winter/config/WebMvcConfigurer.java
@@ -1,7 +1,13 @@
 package com.project.winter.config;
 
+import com.project.winter.mvc.resolver.exception.HandlerExceptionResolver;
+
+import java.util.List;
+
 public interface WebMvcConfigurer {
 
     default void addInterceptors(InterceptorRegistry registry) {}
+
+    default void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {}
 
 }
