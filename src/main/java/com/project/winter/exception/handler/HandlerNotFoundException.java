796026b0e55ed374b464e7084578af5d4412c03b
JUNSEO <djawnstj44@naver.com>
2023-07-31 17:29:45 +0900

    #21 - HandlerNotFoundException 수정
    HandlerNotFoundException 에 기본적으로 ModelAndView 를 가지고 핸들러를 찾지 못할경우 반환할 ModelAndView 를 설정해줬다.

--- src/main/java/com/project/winter/exception/handler/HandlerNotFoundException.java
+++ src/main/java/com/project/winter/exception/handler/HandlerNotFoundException.java
@@ -1,9 +1,15 @@
 package com.project.winter.exception.handler;
 
+import com.project.winter.mvc.view.ModelAndView;
+
 public class HandlerNotFoundException extends HandlerException {
     public HandlerNotFoundException() {}
 
     public HandlerNotFoundException(String message) {
         super(message);
+    }
+
+    public ModelAndView getModelAndView() {
+        return new ModelAndView("404");
     }
 }
